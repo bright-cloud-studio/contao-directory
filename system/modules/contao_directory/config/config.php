8d4c2a1857feb8686da31769c10752d672e43913
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-07 15:27:14 -0400

    Update config.php

--- system/modules/contao_directory/config/config.php
+++ system/modules/contao_directory/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Rep Finder - Location Plugin for Contao
+ * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
  *
- * Copyright (C) 2021 Bright Cloud Studio
+ * Copyright (C) 2022 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/rep-finder
+ * @package    bright-cloud-studio/contao-directory
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
