791fd946713386ecfbf40c777af1a24576b8aaea
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-08 12:16:17 -0400

    Update DirectoryList.php

--- system/modules/contao_directory/library/Bcs/Module/DirectoryList.php
+++ system/modules/contao_directory/library/Bcs/Module/DirectoryList.php
@@ -24,8 +24,6 @@ class DirectoryList extends \Contao\Module
      */
     protected $strTemplate = 'mod_directory_list';
  
-	protected $arrStates = array();
- 
 	/**
 	 * Initialize the object
 	 *
@@ -35,7 +33,6 @@ class DirectoryList extends \Contao\Module
 	public function __construct($objModule, $strColumn='main')
 	{
 		parent::__construct($objModule, $strColumn);
-		$this->arrStates = $this->getStates();
 	}
 	
     /**
@@ -74,28 +71,11 @@ class DirectoryList extends \Contao\Module
 			$this->Template->empty = 'No Listings Found';
 			return;
 		}
-
-		$arrStates = array();
 		
 		// Generate List
 		//while ($objListing->next())
         foreach ($objListing as $objListing)
 		{
-			$strStateKey = $objListing->state;
-			$strStateName = ($this->arrStates["United States"][$objListing->state] != '' ? $this->arrStates["United States"][$objListing->state] : $this->arrStates["Canada"][$objListing->state]);
-			if (in_array($objLocation->state, array('AB','BC','MB','NB','NL','NS','NT','NU','ON','PE','QC','SK','YT'))) {
-				$strStateKey = 'CAN';
-				$strStateName = 'Canada - All Provinces';
-			}
-			
-			if (!array_key_exists($strStateKey, $arrStates)) {
-				$arrStates[$strStateKey] = array(
-					"name" 			=> $strStateName,
-					'pid'			=> $objListing->pid,
-					"abbr"			=> $strStateKey,
-					"listings"		=> array()
-				);
-			}
 			
 			$objListing = array(
 				'id'		=> $objListing->id,
@@ -106,13 +86,6 @@ class DirectoryList extends \Contao\Module
 				'published' => $objListing->published
 			);
 			
-			if ($this->jumpTo) {
-				$objTarget = $this->objModel->getRelated('jumpTo');
-				$arrListing['link'] = $this->generateFrontendUrl($objTarget->row()) .'?alias=' .$objListing->alias;
-			}
-			
-			//$this->Template->categories = \StringUtil::deserialize(YOUR_VARIABLE_HERE);
-			
 			$arrListing['pid']                 = \StringUtil::deserialize($objListing->pid);
 			$arrListing['name']                = $objListing->name;
 			$arrListing['city']                = $objListing->city;
@@ -122,106 +95,9 @@ class DirectoryList extends \Contao\Module
 			$strItemTemplate = ($this->listings_customItemTpl != '' ? $this->listings_customItemTpl : 'item_listing');
 			$objTemplate = new \FrontendTemplate($strItemTemplate);
 			$objTemplate->setData($arrListing);
-			$arrStates[$strStateKey]['listings'][] = $objTemplate->parse();
+			$objTemplate->parse();
 		}
 
-		$arrTemp = $arrStates;
-		unset($arrTemp['CAN']);
-		uasort($arrTemp, array($this,'sortByState'));
-		$arrTemp['CAN'] = $arrStates['CAN'];
-		$arrStates = $arrTemp;
-		
-		$this->Template->stateOptions = $this->generateSelectOptions();
-		$this->Template->states = $arrStates;
-		
 	}
-
-	public function generateSelectOptions($blank = TRUE) {
-		$strUnitedStates = '<optgroup label="United States">';
-		$strCanada = '<optgroup label="Canada"><option value="CAN">All Provinces</option></optgroup>';
-		foreach ($this->arrStates['United States'] as $abbr => $state) {
-			if (!in_array($objListing->state, array('AB','BC','MB','NB','NL','NS','NT','NU','ON','PE','QC','SK','YT'))) {
-				$strUnitedStates .= '<option value="' .$abbr .'">' .$state .'</option>';
-			}
-		}
-		$strUnitedStates .= '</optgroup>';
-		return ($blank ? '<option value="">Select Location...</option>' : '') .$strUnitedStates .$strCanada;
-	}
-	
-	function sortByState($a, $b) {
-		if ($a['Name'] == $b['Name']) {
-			return 0;
-		}
-		return ($a['Name'] < $b['Name']) ? -1 : 1;
-	}
-    
-   function getStates()
-    {		
-        return array(
-			"United States" => array(
-				'AL' => 'Alabama',
-				'AK' => 'Alaska',
-				'AZ' => 'Arizona',
-				'AR' => 'Arkansas',
-				'CA' => 'California',
-				'CO' => 'Colorado',
-				'CT' => 'Connecticut',
-				'DE' => 'Delaware',
-				'FL' => 'Florida',
-				'GA' => 'Georgia',
-				'HI' => 'Hawaii',
-				'ID' => 'Idaho',
-				'IL' => 'Illinois',
-				'IN' => 'Indiana',
-				'IA' => 'Iowa',
-				'KS' => 'Kansas',
-				'KY' => 'Kentucky',
-				'LA' => 'Louisiana',
-				'ME' => 'Maine',
-				'MD' => 'Maryland',
-				'MA' => 'Massachusetts',
-				'MI' => 'Michigan',
-				'MN' => 'Minnesota',
-				'MS' => 'Mississippi',
-				'MO' => 'Missouri',
-				'MT' => 'Montana',
-				'NE' => 'Nebraska',
-				'NV' => 'Nevada',
-				'NH' => 'New Hampshire',
-				'NJ' => 'New Jersey',
-				'NM' => 'New Mexico',
-				'NY' => 'New York',
-				'NC' => 'North Carolina',
-				'ND' => 'North Dakota',
-				'OH' => 'Ohio',
-				'OK' => 'Oklahoma',
-				'OR' => 'Oregon',
-				'PA' => 'Pennsylvania',
-				'RI' => 'Rhode Island',
-				'SC' => 'South Carolina',
-				'SD' => 'South Dakota',
-				'TN' => 'Tennessee',
-				'TX' => 'Texas',
-				'UT' => 'Utah',
-				'VT' => 'Vermont',
-				'VA' => 'Virginia',
-				'WA' => 'Washington',
-				'WV' => 'West Virginia',
-				'WI' => 'Wisconsin',
-				'WY' => 'Wyoming',
-				'AS' => 'American Samoa',
-				'DC' => 'District of Columbia',
-				'FM' => 'Federated States of Micronesia',
-				'GU' => 'Guam',
-				'MH' => 'Marshall Islands',
-				'MP' => 'Northern Mariana Islands',
-				'PW' => 'Palau',
-				'PR' => 'Puerto Rico',
-				'VI' => 'Virgin Islands'),
-			"Canada" => array(
-				"CAN" => "All Provinces")
-			);
-    }
-    
 
 } 
