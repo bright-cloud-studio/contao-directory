010a9b507033c40c3f6569e9219723600248f894
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-08 10:59:08 -0400

    Update modules.php

--- system/modules/contao_directory/languages/en/modules.php
+++ system/modules/contao_directory/languages/en/modules.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * Bright Cloud Studio's Modal Gallery
+ * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
  *
- * Copyright (C) 2021 Bright Cloud Studio
+ * Copyright (C) 2022 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/modal-gallery
+ * @package    bright-cloud-studio/contao-directory
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
-**/
+ */
 
 /* Back end modules */
-$GLOBALS['TL_LANG']['MOD']['modal_gallery'] = array('Modal Gallery', 'Add slides to the modal gallery');
+$GLOBALS['TL_LANG']['MOD']['directory_list']    = array('Directory List', 'Displays Listings and the filters to customize them');
 
 /* Front end modules */
-$GLOBALS['TL_LANG']['FMD']['modal_gallery_module'] 	= array('Modal Gallery', 'The front end module that displays the gallery.');
+$GLOBALS['TL_LANG']['FMD']['listings']          = array('Listings', 'Listings for the Directory');
