566a4fd181b33a1486996b0bccf34b0610fdcd37
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-08 11:07:51 -0400

    Update Listing.php

--- system/modules/contao_directory/library/Bcs/Model/Listing.php
+++ system/modules/contao_directory/library/Bcs/Model/Listing.php
@@ -1,27 +1,25 @@
 <?php
 
 /**
- * Locations - Location Plugin for Contao
+ * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
  *
- * Copyright (C) 2018 Andrew Stevens
+ * Copyright (C) 2022 Bright Cloud Studio
  *
- * @package    asconsulting/locations
- * @link       http://andrewstevens.consulting
+ * @package    bright-cloud-studio/contao-directory
+ * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
  
-namespace Asc\Model;
+namespace Bcs\Model;
 
-class Location extends \Model
+class Listing extends \Model
 {
 	
 	/**
 	 * Table name
 	 * @var string
 	 */
-	protected static $strTable = 'tl_location';
-
-	
+	protected static $strTable = 'tl_listing';
+    
 }
-Footer
