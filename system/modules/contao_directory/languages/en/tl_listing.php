712e952ff444524febbbda456c2097272412f145
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-09 15:02:10 -0400

    Update tl_listing.php

--- system/modules/contao_directory/languages/en/tl_listing.php
+++ system/modules/contao_directory/languages/en/tl_listing.php
@@ -14,19 +14,14 @@
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_listing']['listing_legend']             = 'Listing Main Fields';
-$GLOBALS['TL_LANG']['tl_listing']['address_legend']             = 'Listing Address Fields';
+$GLOBALS['TL_LANG']['tl_listing']['listing_legend']             = 'Main Details';
+$GLOBALS['TL_LANG']['tl_listing']['address_legend']             = 'Address Details';
+$GLOBALS['TL_LANG']['tl_listing']['details_legend']             = 'Specific Details';
+$GLOBALS['TL_LANG']['tl_listing']['specialties_legend']         = 'Specialties';
+$GLOBALS['TL_LANG']['tl_listing']['provide_legend']             = 'Services Provided';
+$GLOBALS['TL_LANG']['tl_listing']['contact_legend']             = 'Contact Details';
+$GLOBALS['TL_LANG']['tl_listing']['internal_legend']            = 'Internal Details';
 $GLOBALS['TL_LANG']['tl_listing']['publish_legend']             = 'Publish';
-
-/* System Fields */
-$GLOBALS['TL_LANG']['tl_listing']['alias']                      = array('Alias', 'Auto-generated alias.');
-$GLOBALS['TL_LANG']['tl_listing']['published']                  = array('Published', 'Show this record on the front end.');
-
-/* Lamvin Unique Fields */
-$GLOBALS['TL_LANG']['tl_listing']['name']                       = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['city']                       = array('Listing City', 'Enter the city of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['state']                      = array('Listing State', 'Enter the state of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['country']                    = array('Listing Countru', 'Enter the country of the listing');
 
  /* Buttons */
 $GLOBALS['TL_LANG']['tl_listing']['new']                        = array('New record', 'Add a new record');
@@ -35,3 +30,52 @@ $GLOBALS['TL_LANG']['tl_listing']['edit']                       = array('Edit re
 $GLOBALS['TL_LANG']['tl_listing']['copy']                       = array('Copy record', 'Copy record ID %s');
 $GLOBALS['TL_LANG']['tl_listing']['delete']                     = array('Delete record', 'Delete record ID %s');
 $GLOBALS['TL_LANG']['tl_listing']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_listing']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_listing']['published']                  = array('Published', 'Show this record on the front end.');
+
+
+
+/* Lamvin Unique Fields */
+$GLOBALS['TL_LANG']['tl_listing']['first_name']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['last_name']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['photo']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+$GLOBALS['TL_LANG']['tl_listing']['city']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['state']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['country']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+$GLOBALS['TL_LANG']['tl_listing']['credentials']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['profession']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['remote_consultations']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['training_program']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['describe_practice']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['specific_services']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+$GLOBALS['TL_LANG']['tl_listing']['specialties_1']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_2']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_3']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_4']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+$GLOBALS['TL_LANG']['tl_listing']['provide_mms']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['provide_cas']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+$GLOBALS['TL_LANG']['tl_listing']['how_to_contact']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['contact_details']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+$GLOBALS['TL_LANG']['tl_listing']['internal_notes']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['date_created']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['date_approved']                       = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['approved']                       = array('Listing Name', 'Enter the name of the listing');
+
+
+
+
+
