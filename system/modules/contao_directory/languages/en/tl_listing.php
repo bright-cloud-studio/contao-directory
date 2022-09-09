b5fb4b0180762dcd352ea7bedd0c15284013b0b5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-09 15:31:46 -0400

    Update tl_listing.php

--- system/modules/contao_directory/languages/en/tl_listing.php
+++ system/modules/contao_directory/languages/en/tl_listing.php
@@ -24,47 +24,50 @@ $GLOBALS['TL_LANG']['tl_listing']['publish_legend']             = 'Publish';
 $GLOBALS['TL_LANG']['tl_listing']['alias']                      = array('Alias', 'Auto-generated alias.');
 $GLOBALS['TL_LANG']['tl_listing']['published']                  = array('Published', 'Show this record on the front end.');
 
+/* Approved Details */
+$GLOBALS['TL_LANG']['tl_listing']['internal_legend']            = 'Approved Status';
+$GLOBALS['TL_LANG']['tl_listing']['approved']                   = array('Approved', 'Select if this listing is Approved and live on the site');
+
 /* Main Details */
 $GLOBALS['TL_LANG']['tl_listing']['listing_legend']             = 'Main Details';
-$GLOBALS['TL_LANG']['tl_listing']['first_name']                 = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['last_name']                  = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['photo']                      = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['first_name']                 = array('First Name', 'Enter the clinician\'s first name');
+$GLOBALS['TL_LANG']['tl_listing']['last_name']                  = array('Last Name', 'Enter the clinician\'s last name');
+$GLOBALS['TL_LANG']['tl_listing']['photo']                      = array('Photo', 'Upload clinician\'s photo');
 
 /* Address Details */
 $GLOBALS['TL_LANG']['tl_listing']['address_legend']             = 'Address Details';
-$GLOBALS['TL_LANG']['tl_listing']['city']                       = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['state']                      = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['country']                    = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['city']                       = array('City', 'Enter the clinician\'s city');
+$GLOBALS['TL_LANG']['tl_listing']['state']                      = array('State', 'Enter the clinician\'s state');
+$GLOBALS['TL_LANG']['tl_listing']['country']                    = array('Country', 'Enter the clinician\'s country');
 
 /* Specific Details */
 $GLOBALS['TL_LANG']['tl_listing']['details_legend']             = 'Specific Details';
-$GLOBALS['TL_LANG']['tl_listing']['credentials']                = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['profession']                 = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['remote_consultations']       = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['training_program']           = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['describe_practice']          = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['specific_services']          = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['credentials']                = array('Credentials', 'Select the clinician\'s credentials');
+$GLOBALS['TL_LANG']['tl_listing']['profession']                 = array('Profession', 'Select the clinician\'s professions');
+$GLOBALS['TL_LANG']['tl_listing']['remote_consultations']       = array('Remote Consultation', 'Select if remote consultation is provided');
+$GLOBALS['TL_LANG']['tl_listing']['training_program']           = array('Training Program', 'Select if the clinician\'s has finished Dr. Ede\'s Training Program');
+$GLOBALS['TL_LANG']['tl_listing']['describe_practice']          = array('Describe Practice', 'Enter a description of the clinician\'s practice');
+$GLOBALS['TL_LANG']['tl_listing']['specific_services']          = array('Specific Services', 'List the clinician\'s specific services offered');
 
 /* Specialties */
 $GLOBALS['TL_LANG']['tl_listing']['specialties_legend']         = 'Specialties';
-$GLOBALS['TL_LANG']['tl_listing']['specialties_1']              = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['specialties_2']              = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['specialties_3']              = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['specialties_4']              = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_1']              = array('Specialties #1', 'List clinician\'s specialties');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_2']              = array('Specialties #2', 'List clinician\'s specialties');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_3']              = array('Specialties #3', 'List clinician\'s specialties');
+$GLOBALS['TL_LANG']['tl_listing']['specialties_4']              = array('Specialties #4', 'List clinician\'s specialties');
 
 /* Services Provided */
 $GLOBALS['TL_LANG']['tl_listing']['provide_legend']             = 'Services Provided';
-$GLOBALS['TL_LANG']['tl_listing']['provide_mms']                = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['provide_cas']                = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['provide_mms']                = array('Medication Management', 'Select if medication management is provided');
+$GLOBALS['TL_LANG']['tl_listing']['provide_cas']                = array('Child/Adolescents Services', 'Select if child/adolescent services are provided');
 
 /* Contact Details */
 $GLOBALS['TL_LANG']['tl_listing']['contact_legend']             = 'Contact Details';
-$GLOBALS['TL_LANG']['tl_listing']['how_to_contact']             = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['contact_details']            = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['how_to_contact']             = array('Contact Method', 'Select prefered method of contact');
+$GLOBALS['TL_LANG']['tl_listing']['contact_details']            = array('Enter Details', 'Enter details for the selected contact method');
 
 /* Internal Details */
 $GLOBALS['TL_LANG']['tl_listing']['internal_legend']            = 'Internal Details';
-$GLOBALS['TL_LANG']['tl_listing']['internal_notes']             = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['date_created']               = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['date_approved']              = array('Listing Name', 'Enter the name of the listing');
-$GLOBALS['TL_LANG']['tl_listing']['approved']                   = array('Listing Name', 'Enter the name of the listing');
+$GLOBALS['TL_LANG']['tl_listing']['internal_notes']             = array('Internal Notes', 'INTERNAL - Notes');
+$GLOBALS['TL_LANG']['tl_listing']['date_created']               = array('Date Created', 'INTERNAL - Date listing created');
+$GLOBALS['TL_LANG']['tl_listing']['date_approved']              = array('Date Approved', 'INTERNAL - Date listing was approved');
