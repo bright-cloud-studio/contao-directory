71e5d82e623fac04fcf481d44244b16c9a8e3d85
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-19 17:27:40 -0400

    Update action.submit.listing.php

--- system/modules/contao_directory/assets/php/action.submit.listing.php
+++ system/modules/contao_directory/assets/php/action.submit.listing.php
@@ -13,9 +13,10 @@
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
     }
+    $sArray = serialize(json_decode($vars['professions']));
   
     
-    $query = "INSERT INTO `tl_listing` (`published`, `country`, `state`, `profession`, `credentials`, `last_name`, `first_name`, `approved`, `specific_services`, `contact_details`, `how_to_contact`, `describe_practice`, `specialties_4`, `specialties_3`, `specialties_2`, `specialties_1`, `provide_cas`, `provide_mms`, `training_program`, `remote_consultations`) VALUES (1, '".$vars['country']."', '".$vars['state']."', '".$vars['professions']."', '".$vars['credentials']."', '".$vars['last_name']."', '".$vars['first_name']."', 'unapproved', '".$vars['specific_services']."', '".$vars['contact_details']."', '".$vars['contact_method']."', '".$vars['desc_practice']."', '".$vars['specialties_4']."', '".$vars['specialties_3']."', '".$vars['specialties_2']."', '".$vars['specialties_1']."', '".$vars['child_services']."', '".$vars['medication_management']."', '".$vars['finished_training']."', '".$vars['remote_consultation']."')";
+    $query = "INSERT INTO `tl_listing` (`published`, `country`, `state`, `profession`, `credentials`, `last_name`, `first_name`, `approved`, `specific_services`, `contact_details`, `how_to_contact`, `describe_practice`, `specialties_4`, `specialties_3`, `specialties_2`, `specialties_1`, `provide_cas`, `provide_mms`, `training_program`, `remote_consultations`) VALUES (1, '".$vars['country']."', '".$vars['state']."', '".$sArray."', '".$vars['credentials']."', '".$vars['last_name']."', '".$vars['first_name']."', 'unapproved', '".$vars['specific_services']."', '".$vars['contact_details']."', '".$vars['contact_method']."', '".$vars['desc_practice']."', '".$vars['specialties_4']."', '".$vars['specialties_3']."', '".$vars['specialties_2']."', '".$vars['specialties_1']."', '".$vars['child_services']."', '".$vars['medication_management']."', '".$vars['finished_training']."', '".$vars['remote_consultation']."')";
     $result = $dbh->prepare($query);
     $result->execute();
 
