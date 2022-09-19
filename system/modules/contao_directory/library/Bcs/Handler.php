cf3022ce13e77bc136ca653f0caf92cd1526b902
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-09-19 17:39:20 -0400

    Create Handler.php

---
+++ system/modules/contao_directory/library/Bcs/Handler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bcs\Handler;
+
+class Handler
+{
+    protected static $arrUserOptions = array();
+
+    public function onProcessForm($submittedData, $formData, $files, $labels, $form)
+    {
+      
+    }
+}
