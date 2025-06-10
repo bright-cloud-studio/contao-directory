283e94404360e23375bcbaf77bad4160ca54249a
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-10 12:31:38 -0400

    Create BcsDirectoryBundle.php

---
+++ src/BcsDirectoryBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Directory
+ * @see	       https://github.com/bright-cloud-studio/contao-directory
+ */
+
+namespace Bcs\DirectoryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsDirectoryBundle extends Bundle
+{
+}
