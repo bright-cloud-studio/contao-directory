5effd0103db4a44b42d87f6d26ef171a9c278d92
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-18 08:37:35 -0400

    Update Handler.php

--- src/Resources/contao/library/Bcs/Handler.php
+++ src/Resources/contao/library/Bcs/Handler.php
@@ -3,6 +3,8 @@
 namespace Bcs;
 
 use Contao\Database;
+use Contao\FilesModel;
+use Contao\StringUtil;
 
 class Handler
 {
@@ -10,9 +12,8 @@ class Handler
 
     public function onProcessForm($submittedData, $formData, $files, $labels, $form)
     {
-
+        
         if($formData['formID'] == 'directory_submission') {
-            
             
             // Create a LOG so we can tell if this is working or not
             $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . '/../files/content/directory/logs/directory_submission_'.strtolower(date('m_d_y_H:m:s')).".txt", "w") or die("Unable to open file!");
@@ -23,10 +24,8 @@ class Handler
             // Close or LOG file
             fclose($myfile);
             
-            
-            $img = \FilesModel::findByUuid($files['photo']['uuid']);
-            $bin = \StringUtil::uuidToBin($files['photo']['uuid']);
-            
+            $img = FilesModel::findByUuid($files['photo']['uuid']);
+            $bin = StringUtil::uuidToBin($files['photo']['uuid']);
             
             $service_area_worldwide = $submittedData['service_area_worldwide'];
             
@@ -50,8 +49,6 @@ class Handler
             } else {
                 $service_area_province = $submittedData['service_area_province'];
             }
-            
-            
             
             if($submittedData['country'] == 'USA')
                 Database::getInstance()->prepare("INSERT INTO tl_listing (`tstamp`, `published`, `approved`, `photo`, `first_name`, `last_name`, `credentials`, `phone`, `email_internal`, `email_public`, `website`, `address_1`, `address_2`, `city`, `state`, `zip`, `country`, `profession`, `remote_consultations`, `training_program`, `describe_practice`, `provide_mms`, `provide_cas`, `language`, `specialties_1`, `specialties_2`, `specialties_3`, `specialties_4`, `how_to_contact`, `specific_services`, `service_area_state`, `service_area_province`, `service_area_country`, `service_area_worldwide`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")->execute(time(), 1, 'unapproved', $bin, $submittedData['first_name'], $submittedData['last_name'], $submittedData['credentials'], $submittedData['phone'], $submittedData['email_internal'], $submittedData['email_public'], $submittedData['website'], $submittedData['address_1'], $submittedData['address_2'], $submittedData['city'], $submittedData['state'], $submittedData['zip'], $submittedData['country'], $submittedData['profession'], $submittedData['remote_consultations'], $submittedData['training_program'], $submittedData['describe_practice'], $submittedData['provide_mms'], $submittedData['provide_cas'], $submittedData['language'], $submittedData['specialties_1'], $submittedData['specialties_2'], $submittedData['specialties_3'], $submittedData['specialties_4'], $submittedData['how_to_contact'], $submittedData['specific_services'], $submittedData['service_area_state'], $submittedData['service_area_province'], $submittedData['service_area_country'], $service_area_worldwide);
